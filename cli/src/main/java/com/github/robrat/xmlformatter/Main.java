c5624a373bcff70d78cdf927e5af5d990e39e060
Robert Rathsack <robert.rathsack@googlemail.com>
2018-12-02 20:26:02 +0100

    added simple main class for xml formatter, added maven shade plugin for cli

---
+++ cli/src/main/java/com/github/robrat/xmlformatter/Main.java
@@ -0,0 +1,32 @@
+package com.github.robrat.xmlformatter;
+
+import com.github.robrat.xmlformatter.lib.XmlConfig;
+import com.github.robrat.xmlformatter.lib.XmlFormatter;
+import java.io.PrintWriter;
+import java.nio.file.Paths;
+
+public class Main {
+
+  public static void main(String[] args) {
+    XmlConfig xmlConfig = XmlConfig.defaults();
+    for (String filename : args) {
+      try {
+        XmlFormatter xmlFormatter = XmlFormatter.ofXmlFile(filename);
+
+        XmlFormatter.FormattedXml formatted = xmlFormatter.format(xmlConfig);
+
+        if (formatted.isModified()) {
+          System.out.println("Formatted " + filename);
+          try (PrintWriter out = new PrintWriter(Paths.get(filename).toFile())) {
+            out.println(formatted.getXml());
+            break;
+          }
+        }
+      } catch (Exception e) {
+        System.err.println("Error while processing " + filename);
+        e.printStackTrace();
+        System.exit(1);
+      }
+    }
+  }
+}
